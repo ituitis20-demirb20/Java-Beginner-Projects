0a5b07ed9f2e62e8fdefcd00118741dc9ff562e0
brkdoc <demirb20@gmail.com>
2021-12-30 12:01:15 +0300

    Update InterestCalculator.java

--- InterestCalculator/src/interestcalculator/InterestCalculator.java
+++ InterestCalculator/src/interestcalculator/InterestCalculator.java
@@ -19,18 +19,42 @@ public class InterestCalculator {
         // TODO code application logic here
         float interestRate;
         float budget;
-        int year;
-        System.out.print("Enter the yearly interest rate: ");
+        int months;
+        System.out.print("Enter the monthly interest rate: ");
         Scanner scanner=new Scanner(System.in);
         interestRate=scanner.nextFloat();
         System.out.print("Enter the money that you want the invest or take as a credit: ");
         budget=scanner.nextFloat();
-        System.out.print("Enter how many year : ");
-        year=scanner.nextInt();
+        System.out.print("Enter how many months : ");
+        months=scanner.nextInt();
         
-        for (int i = 0; i < year; i++) {
+        for (int i = 1; i <= months; i++) {
+            if(i==5){
+                 budget+=(budget*interestRate)/100;
+                 System.out.println((i)+".month money : "+budget);
+            }
+            else if(i==8){
+            System.out.println("Enter 8.month's interest rate");
+            interestRate=scanner.nextInt();
+            scanner.nextLine();
+            
+                budget+=(budget*interestRate)/100;
+                 System.out.println((i)+".month money : "+budget);
+        }
+            else if(i==10){
+            System.out.println("Enter 10.month's interest rate");
+            interestRate=scanner.nextInt();
+            scanner.nextLine();
+            
+                budget+=(budget*interestRate)/100;
+                 System.out.println((i)+".month money : "+budget);
+        }
+            else{
+            
+            
             budget+=(budget*interestRate)/100;
-            System.out.println((i+1)+".year money : "+budget);
+            System.out.println((i)+".month money : "+budget);
+            }
         }
     }
     
